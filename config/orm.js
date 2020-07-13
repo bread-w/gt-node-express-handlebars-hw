e4858b9bf9485500ef776cd439429f83d21f7aa4
Brad Williams <jb.wms89@gmail.com>
2020-07-13 15:04:33 -0400

    continued set up of initial file structure

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(tableInput, cb){
+    connection.query
+    },
+    insertOne: function(){},
+    updateOne: function(){},
+}
