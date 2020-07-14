9fbc2aa06e99a6ef22f25a50ad249f6050292aa1
Brad Williams <jb.wms89@gmail.com>
2020-07-14 09:35:36 -0400

    created updateOne function in the orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,14 @@
 var connection = require("./connection.js");
 
+function objToSql(obj) {
+  var arr = [];
+
+  for (var key in obj) {
+    arr.push(key + "=" + obj[key]);
+  }
+  return arr.toString();
+}
+
 var orm = {
   selectAll: function (tableInput, cb) {
     var readBurgers = "SELECT * FROM " + tableInput + ";";
@@ -8,17 +17,28 @@ var orm = {
       cb(res);
     });
   },
-    insertOne: function (table, data, cb) {
-      connection.query(`INSERT INTO ${table} SET ?`, data, function (err, res) {
-        if (err) {
-          return cb(err);
-        }
-        cb(null, res);
-      });
-    },
-    updateOne: function () {
-      connection.query;
-    },
+  insertOne: function (table, data, cb) {
+    connection.query(`INSERT INTO ${table} SET ?`, data, function (err, res) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, res);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var updateData = "Update" + table;
+    updateData += " SET ";
+    updateData += objToSql(objColVals);
+    updateDate += " WHERE ";
+    updateData += condition;
+
+    connection.query(updateData, function (err, res) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, res);
+    });
+  },
 };
 
 module.exports = orm;
