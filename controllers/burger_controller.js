d9c09ad50d839d86f6f4c33c19f3046d0280c480
Brad Williams <jb.wms89@gmail.com>
2020-07-14 12:47:32 -0400

    added in the update function to change the boolean

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
+const connection = require("../config/connection.js");
 
 router.get("/burger", function (req, res) {
   burger.selectAll(function (data) {
@@ -23,6 +24,18 @@ router.post("/burger", function (req, res) {
   }
 });
 
-router.patch("/burger", function (req, res) {});
+router.put("/burger/:id", function (req, res) {
+  var condition = "id =" + req.params.id;
+  // change boolean value from 0 -> 1
+
+  console.log(condition);
+  burger.updateOne(1, condition, function (err, data) {
+    if (error) {
+      return res.sendStatus(500);
+    }
+    console.log(data);
+    res.sendStatus(201);
+  });
+});
 
 module.exports = router;
